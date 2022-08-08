b74294ea5b3a8c67c0f97f0e97c2839fb2d3c921
Jackie Woodall <jackiewoodall@hotmail.com>
2022-08-08 07:33:44 -0600

    Resolve issues #2 #3

--- barCounter5min.js
+++ barCounter5min.js
@@ -1,13 +1,21 @@
-// barCounter5min v1.0 2022-08-04 JackieW
+// barCounter5min v1.1 2022-08-08 JackieW
 
 // Description:
 //      An Al Brooks style 5min bar count labeler. This custom indicator will
 //      label bar 1 and every even bar in the regular trading cash session on a
 //      5min candlestick chart. Just like Al's setup.
 
+// Release v1.1
+//      o label all bars of the 24 hour session
+//      o hilight hourly bars and bar 18
+
 const predef = require("./tools/predef");
 const meta = require("./tools/meta");
 const {px, du, op} = require('./tools/graphics')
+
+const colorStandard = "#888"
+const color18       = "#0cc"
+const colorHourly   = "#f00"
 
 class BarCounter5min {
     map(d) {
@@ -15,8 +23,11 @@ class BarCounter5min {
         const now = new Date()
         const isToday = now.getDate() == ts.getDate()
         const timestamp5min = Math.floor(((ts.getTime() / 1000) % 86400) / 300)  // convert to UTC epoch seconds and then to a 5min bar count
-        const textValue = timestamp5min - 161   // cash session (RTH) begins at bar 162 (9:30am EST)
-        const shouldDraw = (!this.props.todayOnly || isToday) && textValue > 0 && ((textValue) == 1 || ((textValue+1) % 2))
+        const barCount = timestamp5min - 161   // cash session (RTH) begins at bar 162 (9:30am EST)
+        const textValue = barCount > 0 ? barCount: (300 + barCount)
+        const shouldDraw = (!this.props.todayOnly || isToday) && ((textValue) == 1 || ((textValue+1) % 2))
+
+        const color = (barCount == 18) ? color18 : (barCount%12 == 0) ? colorHourly : colorStandard
         
         return {
             graphics: shouldDraw && {
@@ -32,7 +43,7 @@ class BarCounter5min {
                             x: op(du(d.index()), '-', px(0)),
                             y: op(du(d.low()), '+', px(14))
                         },
-                        style: { fontSize: 12, fill: "#0bf" },
+                        style: { fontSize: 12, fill: color },
                         textAlignment: "centerMiddle"
                     }
                 ]
@@ -51,6 +62,6 @@ module.exports = {
                type: "boolean",
                def: false,
        }
-   },
-   inputType: meta.InputType.BARS,
+    },
+    inputType: meta.InputType.BARS,
 }
